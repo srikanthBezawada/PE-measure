c9bf09c94ecfb60e30907b028d330ab94d8a338f
srikanthBezawada <srikanth.5283@gmail.com>
2016-06-02 16:25:29 +0400

    Updates Help button

--- src/main/java/org/cytoscape/peMeasure/internal/PEhelp.java
+++ src/main/java/org/cytoscape/peMeasure/internal/PEhelp.java
@@ -84,6 +84,7 @@ public class PEhelp extends javax.swing.JFrame {
                 + " - Find the new subnetwork created programmatically \n"
                 + " and the corresponding edges highlighted in the input network \n\n" 
                 + " Now you can use filtered network for predicting complexes"
+                + " You can export the network using File -> Export -> Edgetable  feature"
                 + "----\n";
         
                 
